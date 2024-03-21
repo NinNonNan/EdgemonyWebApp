7d7f427b6795333ada764a3bac91959400a0b91b
nino <nino@isideweb.com>
2024-03-21 13:01:47 +0100

    aggiunto download

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("Qualsiasi cosa");
 
 // STEP 1: getElementById
 const changeImageButton = document.getElementById('changeImageButton');
+const downloadFilteredImageButton = document.getElementById('downloadFilteredImage');
+
 const imageElement = document.getElementById('image');
 const sepiaRange = document.getElementById('sepiaRange');
 const grayscaleRange = document.getElementById('grayscaleRange');
@@ -19,6 +21,8 @@ let isSnyder = false;
 
 // STEP 3: eventi
 changeImageButton.addEventListener('click', changeImage);
+downloadFilteredImageButton.addEventListener('click',downloadFilteredImage);
+
 sepiaRange.addEventListener('input',updateFilters);
 grayscaleRange.addEventListener('input',updateFilters);
 blurRange.addEventListener('input',updateFilters);
@@ -32,7 +36,8 @@ addSnyderFilter.addEventListener('change',snyderFilter);
 // STEP 4: funzioni
 function changeImage() {
     const randomImageId = Math.floor(Math.random() * 1000) + 1;
-    const imageUrl = `https://picsum.photos/id/${randomImageId}/640/480`;
+    //const imageUrl = `https://picsum.photos/id/${randomImageId}/640/480`;
+    const imageUrl = `https://cors-anywhere.herokuapp.com/https://picsum.photos/id/${randomImageId}/640/480`;
     imageElement.src = imageUrl;
     isActive = true;
     toggleFilter.checked = false;
@@ -57,24 +62,55 @@ function updateFilters(){
 
     }
     
-    function resetFilters() {
-        sepiaRange.value = 0;
-        grayscaleRange.value = 0;
-        blurRange.value = 0;
-        brightnessRange.value = 100;
-        contrastRange.value = 100;
-        saturationRange.value = 100;
-        imageElement.style.filter = 'none';
+function resetFilters() {
+
+    sepiaRange.value = 0;
+    grayscaleRange.value = 0;
+    blurRange.value = 0;
+    brightnessRange.value = 100;
+    contrastRange.value = 100;
+    saturationRange.value = 100;
+    imageElement.style.filter = 'none';
+
     }
 
-    function switchFilters() {
-        isActive = !isActive;
-        updateFilters();
+function switchFilters() {
+
+    isActive = !isActive;
+    updateFilters();
+
     }
 
-    function snyderFilter() {
-        isSnyder = !isSnyder;
-        updateFilters();
+function snyderFilter() {
+
+    isSnyder = !isSnyder;
+    updateFilters();
+    }
+
+function downloadFilteredImage() {
+
+    // Seleziona l'elemento del'immagine originale
+    
+    var canvas = document.createElement('canvas');
+    canvas.width = imageElement.width;
+    canvas.height = imageElement.height;
+
+    var ctx = canvas.getContext('2d');
+
+    var stileComputed = window.getComputedStyle(imageElement);
+    var filtro = stileComputed.getPropertyValue('filter');
+    ctx.filter = filtro; // Applica lo stesso filtro sull'immagin
+
+    // Disegna l'immagine con il filtro applicato sul canvas
+    ctx.drawImage(imageElement, 0, 0);
+
+    // Crea un link per il download
+    var link = document.createElement('a');
+    link.download = 'immagine_filtrata.jpg'; // Nome del file da scaricare
+    // Converti il canvas in un URL di dati
+    link.href = canvas.toDataURL('image/jpeg');
+    // Clicca automaticamente sul link per avviare il download
+    link.click();
     }
 
     changeImage();
