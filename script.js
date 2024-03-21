87be9bc3708bee9762526c377f82bead27f35f21
nino <nino@isideweb.com>
2024-03-21 12:26:58 +0100

    Aggiunta SnyderMode

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 console.log("Qualsiasi cosa");
 
 // STEP 1: getElementById
-const changeImageButton = document.getElementById("changeImageButton");
-const imageElement = document.getElementById("image");
-const sepiaRange = document.getElementById("sepiaRange");
-const grayscaleRange = document.getElementById("grayscaleRange");
-const blurRange = document.getElementById("blurRange");
-const brightnessRange = document.getElementById("brightnessRange");
-const contrastRange = document.getElementById("contrastRange");
-const saturationRange = document.getElementById("saturationRange");
+const changeImageButton = document.getElementById('changeImageButton');
+const imageElement = document.getElementById('image');
+const sepiaRange = document.getElementById('sepiaRange');
+const grayscaleRange = document.getElementById('grayscaleRange');
+const blurRange = document.getElementById('blurRange');
+const brightnessRange = document.getElementById('brightnessRange');
+const contrastRange = document.getElementById('contrastRange');
+const saturationRange = document.getElementById('saturationRange');
 const toggleFilter = document.getElementById('toggleFilter');
+// STEP 1.1 SPERIMENTALE
+const addSnyderFilter = document.getElementById('addSnyderFilter');
 
 // STEP 2: variabili
 let isActive = true;
+let isSnyder = false;
 
 // STEP 3: eventi
 changeImageButton.addEventListener('click', changeImage);
-sepiaRange.addEventListener("input",updateFilters);
-grayscaleRange.addEventListener("input",updateFilters);
-blurRange.addEventListener("input",updateFilters);
-brightnessRange.addEventListener("input",updateFilters);
-contrastRange.addEventListener("input",updateFilters);
-saturationRange.addEventListener("input",updateFilters);
+sepiaRange.addEventListener('input',updateFilters);
+grayscaleRange.addEventListener('input',updateFilters);
+blurRange.addEventListener('input',updateFilters);
+brightnessRange.addEventListener('input',updateFilters);
+contrastRange.addEventListener('input',updateFilters);
+saturationRange.addEventListener('input',updateFilters);
 toggleFilter.addEventListener('change', switchFilters);
+// STEP 3.1 
+addSnyderFilter.addEventListener('change',snyderFilter);
 
 // STEP 4: funzioni
 function changeImage() {
@@ -48,6 +53,7 @@ function updateFilters(){
 
         // STEP 5: Condizioni - attenzione al ternario!!!
         imageElement.style.filter = isActive ? filters : 'none';
+        imageElement.style.filter = isSnyder ? `saturate(10%)` : filters;
 
     }
     
@@ -63,6 +69,11 @@ function updateFilters(){
 
     function switchFilters() {
         isActive = !isActive;
+        updateFilters();
+    }
+
+    function snyderFilter() {
+        isSnyder = !isSnyder;
         updateFilters();
     }
 
